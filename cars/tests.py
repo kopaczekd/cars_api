84b62781c72aa25f462f5216a803c56323e6db28
kopaczekd <kopaczekd@gmail.com>
2021-06-23 00:01:42 +0200

    Fix test after reorganization whole project

--- cars/tests.py
+++ cars/tests.py
@@ -2,13 +2,12 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
-from .models import Car
-from rate.models import Rate
+from .models import Car, Rate
 
 
 class CarAPITest(APITestCase):
     def setUp(self):
-        self.url_list_create = reverse('cars:list_create')
+        self.url_list_create = reverse('cars:cars_list_create')
         self.car = Car.objects.create(make='Lamborghini', model='Gallardo')
         Car.objects.create(make='Volkswagen', model='Passat')
 
@@ -38,14 +37,14 @@ class CarAPITest(APITestCase):
         response_get_before_deletion = self.client.get(self.url_list_create)
         self.assertEqual(len(response_get_before_deletion.data), 2)
         first_car_id = response_get_before_deletion.data[0]['id']
-        url_delete = reverse('cars:delete', args=[first_car_id])
+        url_delete = reverse('cars:cars_delete', args=[first_car_id])
         response_deletion = self.client.delete(url_delete)
         self.assertEqual(response_deletion.status_code, status.HTTP_204_NO_CONTENT)
         response_get_after_deletion = self.client.get(self.url_list_create)
         self.assertEqual(len(response_get_after_deletion.data), 1)
 
     def test_car_deletion_failure(self):
-        url_delete = reverse('cars:delete', args=[777])
+        url_delete = reverse('cars:cars_delete', args=[777])
         response_deletion = self.client.delete(url_delete)
         self.assertEqual(response_deletion.status_code, status.HTTP_404_NOT_FOUND)
 
@@ -60,7 +59,7 @@ class CarAPITest(APITestCase):
 
 class PopularAPITests(APITestCase):
     def setUp(self):
-        url_list_create_rate = reverse('rate:list_create')
+        url_list_create_rate = reverse('cars:rate_list_create')
         self.car_1 = Car.objects.create(make='Lamborghini', model='Gallardo')
         data_1 = {
             'car_id': self.car_1.id,
@@ -81,7 +80,7 @@ class PopularAPITests(APITestCase):
         self.client.post(url_list_create_rate, data_2)
 
     def test_taking_all_popular_cars(self):
-        url_popular = reverse('popular:list')
+        url_popular = reverse('cars:popular_list')
         response = self.client.get(url_popular)
         self.assertEqual(response.data[0]['id'], self.car_2.id)
         self.assertEqual(response.data[0]['rates_number'], 5)
@@ -93,7 +92,7 @@ class PopularAPITests(APITestCase):
 class RateAPITest(APITestCase):
     def setUp(self):
         self.car = Car.objects.create(make='Lamborghini', model='Gallardo')
-        self.url_list_create = reverse('rate:list_create')
+        self.url_list_create = reverse('cars:rate_list_create')
         Rate.objects.create(car_id=self.car, rating=5)
 
     def test_rate_creation_success(self):
