5c19943a0324359c5a06f641bdd3e4a79187eeaf
kopaczekd <kopaczekd@gmail.com>
2021-06-23 20:33:08 +0200

    Add Rate model do admin panel

--- cars/admin.py
+++ cars/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Car
+from .models import Car, Rate
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     list_display = ['id', 'make', 'model', 'rates_number']
     list_filter = ('make',)
+
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'car_id', 'rating']
